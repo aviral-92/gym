609e8dd873a184db3e84ca1afc67a6a8813c681f
Aviral Mittal <aviralmittal2012@gmail.com>
2020-07-30 22:54:10 -0400

    Update RestApiService.dart

--- lib/services/RestApiService.dart
+++ lib/services/RestApiService.dart
@@ -92,7 +92,7 @@ Future<AdtItemSlotsBooked> slotBooking(
 
 var storage = FlutterSecureStorage();
 String value =
-    '[{"key":"Authorization","value":"Bearer eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCJ9.eyJleHAiOjE1OTYxMTM4MDksInVzZXJfbmFtZSI6InVzZXIiLCJhdXRob3JpdGllcyI6WyJST0xFX1VTRVIiXSwianRpIjoiNzljOTk4MGMtNWZjNi00NDVkLTk4YTEtYzA4N2Y5ZDJjOWM1IiwiY2xpZW50X2lkIjoiY2xpZW50SWQiLCJzY29wZSI6WyJyZWFkIiwid3JpdGUiXX0.jsbnZs6asxw38SSF3fDlN84V2GRujO9FLoGaNgrwer6QEVshZFIMHARhUlQaCpM670-DDKHB2fPXgsoraUO9qsgFk26EulEB7yWFk8S_m7o4TlBGrhMOUoIdjcR-aBBHaNIJZ1uXxS6_gXPwwrfMEIqVQUgT6yEiQjIGfymVuEzjAxeBHu7d1h75U2W1FtRSTbBIv8iawBCpIsILMgCO0ZArgxAw6MRmM9TeBtga4NqRcx4AY93hkGH2r801HKYYSfiFLmsHq4R1Ts3c4uV2CmedK3Ob63rISHtKWTCFe0MnbkH3kjMXUjcEH-aK_cGHLJJiPaEXy-k_4yVFv6v30w","description":"","type":"text","enabled":true}]';
+    '[{"key":"Authorization","value":"Bearer eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCJ9.eyJleHAiOjE1OTYyNDg2MzksInVzZXJfbmFtZSI6InVzZXIiLCJhdXRob3JpdGllcyI6WyJST0xFX1VTRVIiXSwianRpIjoiYjUzYzkwYjktYzY2ZS00MmFiLTljNWUtZmRjMWIyY2ZkN2E1IiwiY2xpZW50X2lkIjoiY2xpZW50SWQiLCJzY29wZSI6WyJyZWFkIiwid3JpdGUiXX0.FAM1w8nS-FBDN1HUVwkB-pg3etSjVCzAJMZnshl-sRWa30YC6bNEmEQ3E5FJmYtPIFtVw5h8E4QPtFMvDjJAjzsIN0fvvbtKm_nuFjUpD3DkWL8B2oVGn-NCM2Th7IG4pQ2ARCrHlSRktPczlybzb86Fo79H2LZ9b9N-XO1u6eozLuX96fBNLvl3dq4gei0tzZgfJ4pxJkGOMPtVJmlummXMN1H9bQgOnuaVLx_QzYNXOvs2SSIlvRZtnTIQsVS53Y_YIFIa2l3V9pucqaxOml0s_o0-5vaWCOti-PRWeJtsub-tWjfLjWgG-504dV9a5aN2oDdQOmBBzWZAEOUcyg":"","type":"text","enabled":true}]';
 void addStorage() {
   print('------Add Storage method called-------');
   storage.write(key: "Authorization", value: value);
